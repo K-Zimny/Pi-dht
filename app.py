3a37e0f22b3ce751647b7f52ccdfb0e66608a728
KZ <kenneth.zimny@gmail.com>
2025-07-03 08:19:45 -0500

    Refactors

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ def get_db():
     db = getattr(g, '_database', None)
     if db is None:
         db = g._database = sqlite3.connect(DATABASE)
+        db.row_factory = sqlite3.Row
     return db
 
 @app.teardown_appcontext
@@ -23,7 +24,12 @@ def close_connection(exception):
 def web_app():
     cur = get_db().cursor()
 
-    history = cur.execute("SELECT currentdate, temperature, humidity FROM dhtreadings").fetchall()
+    history = cur.execute("SELECT currentdate, temperature, humidity FROM dhtreadings ORDER BY id DESC").fetchall()
+    if history is None:
+        history = {"currentdata": "N/A", "temperature": "N/A", "humidity": "N/A"}
+
     latest = cur.execute("SELECT temperature, humidity FROM dhtreadings ORDER BY id DESC LIMIT 1").fetchone()
+    if latest is None:
+        latest = {"temperature": "N/A", "humidity": "N/A"}
 
     return render_template("index.html", history=history, latest=latest)
